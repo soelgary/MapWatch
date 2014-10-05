cce8682fe6d5a5c8615e04406869c83a7f728215
Gary Soeller <soeller.g@husky.neu.edu>
2014-10-05 18:52:09 -0400

    run basic job every 5 seconds at the start

--- MapsService/src/main/java/com/gsoeller/personalization/maps/MapsApplication.java
+++ MapsService/src/main/java/com/gsoeller/personalization/maps/MapsApplication.java
@@ -1,8 +1,18 @@
 package com.gsoeller.personalization.maps;
 
+import org.quartz.JobBuilder;
+import org.quartz.JobDetail;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.quartz.SchedulerFactory;
+import org.quartz.SimpleScheduleBuilder;
+import org.quartz.Trigger;
+import org.quartz.TriggerBuilder;
+import org.quartz.impl.StdSchedulerFactory;
 import org.skife.jdbi.v2.DBI;
 
 import com.gsoeller.personalization.maps.dao.MapRequestDao;
+import com.gsoeller.personalization.maps.jobs.FetchJob;
 import com.gsoeller.personalization.maps.resources.MapsResource;
 
 import io.dropwizard.Application;
@@ -14,26 +24,53 @@ import io.dropwizard.setup.Environment;
 
 public class MapsApplication extends Application<MapsConfiguration> {
 
+	private MapRequestDao mapRequestDao;
+	
 	public static void main(String[] args) throws Exception {
 		new MapsApplication().run(args);
 	}
-	
+
 	@Override
 	public void initialize(Bootstrap<MapsConfiguration> bootstrap) {
-		 bootstrap.addBundle(new MigrationsBundle<MapsConfiguration>() {
-		            public DataSourceFactory getDataSourceFactory(MapsConfiguration configuration) {
-		                return configuration.getDataSourceFactory();
-		            }
-		    });
+		bootstrap.addBundle(new MigrationsBundle<MapsConfiguration>() {
+			public DataSourceFactory getDataSourceFactory(
+					MapsConfiguration configuration) {
+				return configuration.getDataSourceFactory();
+			}
+		});
 	}
 
 	@Override
 	public void run(MapsConfiguration config, Environment environment)
 			throws Exception {
 		final DBIFactory factory = new DBIFactory();
-	    final DBI jdbi = factory.build(environment, config.getDataSourceFactory(), "mysql");
-	    MapRequestDao mapRequestDao = jdbi.onDemand(MapRequestDao.class);
-	    environment.jersey().register(new MapsResource(mapRequestDao));
+		final DBI jdbi = factory.build(environment,
+				config.getDataSourceFactory(), "mysql");
+		mapRequestDao = jdbi.onDemand(MapRequestDao.class);
+		environment.jersey().register(new MapsResource(mapRequestDao));
+		startFetchJob();
+	}
+
+	private void startFetchJob() throws SchedulerException {
+		SchedulerFactory schedFact = new StdSchedulerFactory();
+		Scheduler sched = schedFact.getScheduler();
+		sched.start();
+
+		JobDetail job = JobBuilder.newJob(FetchJob.class)
+				.withIdentity("Fetch Job", "group1").build();
+		
+		job.getJobDataMap().put("MapRequestDao", mapRequestDao);
+
+		Trigger trigger = TriggerBuilder
+				.newTrigger()
+				.withIdentity("Fetch Trigger", "group1")
+				.startNow()
+				.withSchedule(SimpleScheduleBuilder.simpleSchedule()
+								.withIntervalInSeconds(5).repeatForever())
+				.build();
+		
+
+		sched.scheduleJob(job, trigger);
 	}
 
 }
