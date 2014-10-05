3a2ebf1e43f087f7db2c9b5ed959b3f81439e90c
Gary Soeller <soeller.g@husky.neu.edu>
2014-10-05 15:16:18 -0400

    temporary endpoint to send a request

--- MapsService/src/main/java/com/gsoeller/personalization/maps/resources/MapsResource.java
+++ MapsService/src/main/java/com/gsoeller/personalization/maps/resources/MapsResource.java
@@ -8,7 +8,9 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import com.gsoeller.personalization.maps.StaticMapFetcher;
 import com.gsoeller.personalization.maps.dao.MapRequestDao;
+import com.gsoeller.personalization.maps.data.Map;
 import com.gsoeller.personalization.maps.data.MapRequest;
 
 @Path("/maps")
@@ -16,6 +18,7 @@ import com.gsoeller.personalization.maps.data.MapRequest;
 public class MapsResource {
 	
 	private final MapRequestDao dao;
+	private final StaticMapFetcher fetcher = new StaticMapFetcher();
 	
 	public MapsResource(MapRequestDao dao) {
 		this.dao = dao;
@@ -23,13 +26,22 @@ public class MapsResource {
 	
 	@GET
 	public List<MapRequest> getMapRequests() {
-		return dao.getRequests();
+		List<MapRequest> request = dao.getRequests();
+		Map map = new Map.MapBuilder().setMapRequest(request.get(0)).build();
+		System.out.println(request.get(0).buildRequestUrl());
+		return request;
 	}
 	
 	@POST
 	public MapRequest addRequest(MapRequest mapRequest) {
 		dao.addMapRequest(mapRequest.getLatitude(), mapRequest.getLongitude(), mapRequest.getZoom(), mapRequest.getxDimension(), mapRequest.getyDimension(), mapRequest.getRegion().toString(), mapRequest.getLanguage().toString());
 		return dao.getRequests().get(0);
+	}
+	
+	@POST
+	@Path("/send")
+	public void sendRequest() {
+		fetcher.fetch(getMapRequests().get(0));
 	}
 
 }
