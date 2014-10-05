1b9342d9ead1faee6fa13737dc2dfeb7a480d5f3
Gary Soeller <soeller.g@husky.neu.edu>
2014-10-05 15:15:49 -0400

    add method to fetch a map using a MapRequest object

--- MapsService/src/main/java/com/gsoeller/personalization/maps/StaticMapFetcher.java
+++ MapsService/src/main/java/com/gsoeller/personalization/maps/StaticMapFetcher.java
@@ -7,32 +7,36 @@ import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.joda.time.DateTime;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 
 import com.gsoeller.personalization.maps.data.ImageDao;
+import com.gsoeller.personalization.maps.data.Map;
+import com.gsoeller.personalization.maps.data.Map.MapBuilder;
+import com.gsoeller.personalization.maps.data.MapRequest;
 
 public class StaticMapFetcher {
-	private static final String API_KEY =  "AIzaSyCuoblPc_plcMWIOGi7C5Td2mRQccxkpuc";
-	private static final String STATIC_MAP_API_ENDPOINT = "http://maps.googleapis.com/maps/api/staticmap?center=26,78&zoom=5&size=600x600&sensor=true&region=en&language=english&key=" + API_KEY;
+	private final String API_KEY = "AIzaSyCuoblPc_plcMWIOGi7C5Td2mRQccxkpuc";
+	private final String STATIC_MAP_API_ENDPOINT = "http://maps.googleapis.com/maps/api/staticmap?center=26,78&zoom=5&size=600x600&sensor=true&region=en&language=english&key="
+			+ API_KEY;
 	private final CloseableHttpClient client = HttpClients.createDefault();
 	private final ImageDao imageDao = new ImageDao();
-	
-	
-	private static final Logger LOG = LoggerFactory.getLogger(StaticMapFetcher.class);
-	
-	
+
+	private static final Logger LOG = LoggerFactory
+			.getLogger(StaticMapFetcher.class);
+
 	public void fetch(String filename) {
-		HttpGet request  = new HttpGet(STATIC_MAP_API_ENDPOINT);
+		HttpGet request = new HttpGet(STATIC_MAP_API_ENDPOINT);
 		try {
 			CloseableHttpResponse response = client.execute(request);
-			if(response.getStatusLine().getStatusCode() == 200) {
+			if (response.getStatusLine().getStatusCode() == 200) {
 				LOG.info("Successfully made a request to the Static Maps API");
 				imageDao.saveImage(filename + ".png", response.getEntity());
 			} else {
 				LOG.error("Request to Static Maps API Failed");
 				LOG.error(response.getStatusLine().getReasonPhrase());
-			}			
+			}
 		} catch (ClientProtocolException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -40,5 +44,27 @@ public class StaticMapFetcher {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+
+	public Map fetch(MapRequest request) {
+		HttpGet httpGet = new HttpGet(request.buildRequestUrl());
+		try {
+			CloseableHttpResponse response = client.execute(httpGet);
+			if (response.getStatusLine().getStatusCode() == 200) {
+				LOG.info("Successfully made a request to the Static Maps API");
+				MapBuilder mapBuilder = new Map.MapBuilder().setDateTime(DateTime.now()).setMapRequest(request).setHasChanged(false);
+				System.out.println(response.getEntity());
+			} else {
+				LOG.error("Request to Static Maps API Failed");
+				LOG.error(response.getStatusLine().getReasonPhrase());
+			}
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
 	}
 }
