a17c225702bd55c60f016ff8fd7d1f194ac53dbe
Gary Soeller <soeller.g@husky.neu.edu>
2014-10-04 21:38:29 -0400

    oops

---
+++ MapsService/src/main/java/com/gsoeller/personalization/maps/data/ImageDao.java
@@ -0,0 +1,31 @@
+package com.gsoeller.personalization.maps.data;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.apache.http.HttpEntity;
+
+public class ImageDao {
+
+	private static final String OUTPUT_DIR = "/Users/garysoeller/dev/src/MapsPersonalization/MapsService/src/main/resources/";
+	//private static final String OUTPUT_DIR = "/home/soelgary/Maps";
+
+	public void saveImage(String filename, HttpEntity entity) {
+		File imageFile = new File(OUTPUT_DIR + filename);
+		try {
+			FileOutputStream foutStream = new FileOutputStream(imageFile);
+			entity.writeTo(foutStream);
+			foutStream.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
