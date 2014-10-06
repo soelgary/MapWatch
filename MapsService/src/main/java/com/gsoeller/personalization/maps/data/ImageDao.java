5b09db2d8624fc2d3516832219e62509cd806be9
Gary Soeller <soeller.g@husky.neu.edu>
2014-10-05 22:13:46 -0400

    functionality for adding and removing images

--- MapsService/src/main/java/com/gsoeller/personalization/maps/data/ImageDao.java
+++ MapsService/src/main/java/com/gsoeller/personalization/maps/data/ImageDao.java
@@ -1,16 +1,20 @@
 package com.gsoeller.personalization.maps.data;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import javax.imageio.ImageIO;
 
 import org.apache.http.HttpEntity;
 
 public class ImageDao {
 
 	private static final String OUTPUT_DIR = "/Users/garysoeller/dev/src/MapsPersonalization/MapsService/src/main/resources/";
-	//private static final String OUTPUT_DIR = "/home/soelgary/Maps";
 
 	public void saveImage(String filename, HttpEntity entity) {
 		File imageFile = new File(OUTPUT_DIR + filename);
@@ -25,7 +29,24 @@ public class ImageDao {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
 	}
-
+	
+	public BufferedImage getImage(String name) {
+		BufferedImage img;
+		try {
+			img = ImageIO.read(new File(OUTPUT_DIR + name));
+			return img;
+		} catch (IOException e) {
+			throw new RuntimeException("An IOException occurred while reading an image");
+		}
+	}
+	
+	public void removeImage(String name) {
+		try {
+			Files.deleteIfExists(Paths.get(OUTPUT_DIR + name));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
