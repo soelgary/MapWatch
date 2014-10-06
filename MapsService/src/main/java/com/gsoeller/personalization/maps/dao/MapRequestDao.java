667a269953ec82ea99edc90578082f8e150f4b33
Gary Soeller <soeller.g@husky.neu.edu>
2014-10-05 22:13:09 -0400

    generate keys on update and query by id

--- MapsService/src/main/java/com/gsoeller/personalization/maps/dao/MapRequestDao.java
+++ MapsService/src/main/java/com/gsoeller/personalization/maps/dao/MapRequestDao.java
@@ -3,13 +3,13 @@ package com.gsoeller.personalization.maps.dao;
 import java.util.List;
 
 import org.skife.jdbi.v2.sqlobject.Bind;
+import org.skife.jdbi.v2.sqlobject.GetGeneratedKeys;
 import org.skife.jdbi.v2.sqlobject.SqlQuery;
 import org.skife.jdbi.v2.sqlobject.SqlUpdate;
 import org.skife.jdbi.v2.sqlobject.customizers.Mapper;
 
-import com.gsoeller.personalization.maps.data.Language;
+import com.google.common.base.Optional;
 import com.gsoeller.personalization.maps.data.MapRequest;
-import com.gsoeller.personalization.maps.data.Region;
 import com.gsoeller.personalization.maps.mappers.MapRequestMapper;
 
 public interface MapRequestDao {
@@ -17,7 +17,11 @@ public interface MapRequestDao {
 	@SqlQuery("select * from MapRequest")
 	@Mapper(MapRequestMapper.class)
 	public List<MapRequest> getRequests();
+	
+	@SqlQuery("select * from MapRequest where id=:id")
+	public Optional<MapRequest> getRequest(@Bind("id") int id);
 
 	@SqlUpdate("insert into MapRequest (latitude, longitude, zoom, xDimension, yDimension, region, language) values (:latitude, :longitude, :zoom, :xDimension, :yDimension, :region, :language)")
+	@GetGeneratedKeys
 	public int addMapRequest(@Bind("latitude") double latitude, @Bind("longitude") double longitude, @Bind("zoom") int zoom, @Bind("xDimension") int xDimension, @Bind("yDimension") int yDimension, @Bind("region") String region, @Bind("language") String language);
 }
