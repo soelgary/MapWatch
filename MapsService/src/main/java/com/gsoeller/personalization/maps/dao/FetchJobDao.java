6f2259bc77102e8776f55fb180c7e4da011fddc3
Gary Soeller <soeller.g@husky.neu.edu>
2014-11-04 22:38:19 -0500

    query for jobs in desc order to make it easy to access latest id's

--- MapsService/src/main/java/com/gsoeller/personalization/maps/dao/FetchJobDao.java
+++ MapsService/src/main/java/com/gsoeller/personalization/maps/dao/FetchJobDao.java
@@ -1,6 +1,9 @@
 package com.gsoeller.personalization.maps.dao;
 
+import java.util.List;
+
 import org.skife.jdbi.v2.sqlobject.GetGeneratedKeys;
+import org.skife.jdbi.v2.sqlobject.SqlQuery;
 import org.skife.jdbi.v2.sqlobject.SqlUpdate;
 
 public interface FetchJobDao {
@@ -8,4 +11,8 @@ public interface FetchJobDao {
 	@SqlUpdate("Insert into FetchJob (id) values (null)")
 	@GetGeneratedKeys
 	public int createFetchJob();
+
+	@SqlQuery("select id from fetchjob order by id desc")
+	public List<Integer> getFetchJobs();	
+	
 }
