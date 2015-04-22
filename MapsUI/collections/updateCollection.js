30e7962ada2d81010bd131e4e07bf5b3ddd1e95a
Gary Soeller <soeller.g@husky.neu.edu>
2015-04-21 23:13:31 -0700

    parse correctly

--- MapsUI/collections/updateCollection.js
+++ MapsUI/collections/updateCollection.js
@@ -3,7 +3,9 @@ define(["models/updateModel.js"],
     var Updates = Backbone.Collection.extend({
       model: Update,
       async: false,
-      url: 'http://' + window.location.hostname + ':' + window.location.port + '/maps/google/updates?reserve=false',
+      url: 'http://achtung.ccs.neu.edu:9092/maps/google/hits/109',
+      //url: 'http://' + window.location.hostname + ':' + window.location.port + '/maps/google/updates?reserve=false',
+      //url: 'http://127.0.0.1:9092/maps/google/updates?reserve=false',
       parse : function(resp) {
         return resp;
       },
@@ -12,16 +14,20 @@ define(["models/updateModel.js"],
             success: this.fetchSuccess,
             error: this.fetchError
         });
-    },
+      },
 
-    fetchSuccess: function (collection, response) {
-        console.log('Collection fetch success', response);
-        console.log('Collection models: ', collection.models);
-    },
+      fetchSuccess: function (collection, response) {
+          console.log('Collection fetch success', response);
+          console.log('Collection models: ', collection.models);
+      },
 
-    fetchError: function (collection, response) {
-        throw new Error("Maps fetch error");
-    }
+      fetchError: function (collection, response) {
+          throw new Error("Maps fetch error");
+      },
+
+      parse: function(response) {
+        return response.updates;
+      }
     });
   return Updates;
 });
