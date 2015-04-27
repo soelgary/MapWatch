b0553f5c467c48c124e01dd5e66ba1838e327f0d
Gary Soeller <soeller.g@husky.neu.edu>
2015-04-26 23:00:22 -0700

    parse hitId in update model

--- MapsUI/models/updateModel.js
+++ MapsUI/models/updateModel.js
@@ -2,8 +2,21 @@ define([],
   function(){
     var Update = Backbone.Model.extend({
       url: function() {
-        return 'http://achtung.ccs.neu.edu:9092/maps/google/hits/mturk/' + this.hitId + '/update/' + this.id;
+        return 'http://achtung.ccs.neu.edu:9092/maps/google/hits/mturk/' + this.getHITId() + '/update/' + this.id;
       },
+
+      getHITId: function() {
+        var query = window.location.search.substring(1);
+        var vars = query.split("&");
+        for (var i=0;i<vars.length;i++) {
+          var pair = vars[i].split("=");
+          if(pair[0] == 'hitId'){
+            return pair[1];
+          }
+       }
+       return(false);
+      },
+
       defaults: {
         oldMap: null,
         newMap: null,
