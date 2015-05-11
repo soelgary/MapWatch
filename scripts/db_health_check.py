79924a45db8d7a91278302c13fc92003935677ed
Gary Soeller <soeller.g@husky.neu.edu>
2015-05-10 18:15:01 -0700

    oops

--- scripts/db_health_check.py
+++ scripts/db_health_check.py
@@ -1,7 +1,7 @@
 import requests
 import time
 
-url = "http://127.0.0.1:9092maps/google/hits?approved=true&count=1000"
+url = "http://127.0.0.1:9092/maps/google/hits?approved=true&count=1000"
 ten_minutes = 60 * 10
 
 while True:
